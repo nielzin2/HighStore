da1e8b3cc1aa8336c3145aede7d894ad6109484e
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-12 11:05:08 -0300

    Update wsgi.py

--- almoxarifado_web/wsgi.py
+++ almoxarifado_web/wsgi.py
@@ -1,1 +1,14 @@
+"""
+WSGI config for almoxarifado_web project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# Define o módulo de configurações do Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'almoxarifado_web.settings')
+
+application = get_wsgi_application()
