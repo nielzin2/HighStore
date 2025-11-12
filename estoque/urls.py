50f1231bb768f99f83ce80789b211a577ea019d1
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-12 08:03:50 -0300

    Add URL routes for item listing and adding
    HS-02: Atualiza estoque/urls.py com a rota para adicionar item.

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from . import views # Importa as views (funções de lógica) da aplicação atual
+from . import views
 
-# Define o namespace da aplicação para evitar conflitos de nomes
 app_name = 'estoque'
 
 urlpatterns = [
-    # path('', views.home, name='home'), # Exemplo de rota para uma homepage
-    
-    # Rota para a Listagem de Itens (F-03)
-    # Quando a URL for a raiz da app (ex: /), chama a view 'lista_itens'
+    # Rota 1: Listagem de Itens (Index - CRUD R)
     path('', views.lista_itens, name='lista_itens'), 
+    
+    # Rota 2: Adicionar Novo Item (CRUD C)
+    path('novo/', views.adicionar_item, name='adicionar_item'), 
 ]
