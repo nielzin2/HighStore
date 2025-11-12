55d3dcc83b881ea3f1486b33663bb53da3e26cc5
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-12 09:55:36 -0300

    Add route for editing specific item
    HS-04: Atualiza estoque/urls.py com a rota para editar item (<int:pk>).

--- estoque/urls.py
+++ estoque/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     
     # Rota 2: Adicionar Novo Item (CRUD C)
     path('novo/', views.adicionar_item, name='adicionar_item'), 
+    
+    # Rota 3: Editar Item Específico (CRUD U)
+    # <int:pk> captura um número inteiro (a Primary Key) e passa para a view
+    path('editar/<int:pk>/', views.editar_item, name='editar_item'),
 ]
