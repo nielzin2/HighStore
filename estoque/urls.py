60b2410d9edc62734be52e9415a87e9ff8e320ad
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-17 13:55:11 -0300

    Update urls.py
    HS-14: Adiciona rotas para registrar e listar movimentações.

--- estoque/urls.py
+++ estoque/urls.py
@@ -4,18 +4,15 @@ from . import views
 app_name = 'estoque'
 
 urlpatterns = [
-    # Rota 1: Listagem de Itens (Index - CRUD R)
     path('', views.lista_itens, name='lista_itens'), 
-    
-    # Rota 2: Adicionar Novo Item (CRUD C)
     path('novo/', views.adicionar_item, name='adicionar_item'), 
-    
-    # Rota 3: Editar Item Específico (CRUD U)
     path('editar/<int:pk>/', views.editar_item, name='editar_item'),
-    
-    # Rota 4: Excluir Item Específico (CRUD D)
     path('deletar/<int:pk>/', views.deletar_item, name='deletar_item'),
-    
-    # Rota 5: NOVO - Alerta de Estoque Mínimo (HS-12)
     path('alerta/', views.alerta_estoque, name='alerta_estoque'),
+    
+    # Rota 6: NOVO - Registrar Movimentação para um Item Específico (HS-14)
+    path('movimentar/<int:pk>/', views.registrar_movimentacao, name='registrar_movimentacao'),
+    
+    # Rota 7: NOVO - Listar Histórico de Movimentações (HS-14)
+    path('historico/', views.historico_movimentacoes, name='historico_movimentacoes'),
 ]
