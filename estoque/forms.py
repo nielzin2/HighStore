857811d0b414ce3991ea06265d557b3e8dd521e6
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-17 13:54:29 -0300

    Update forms.py
    HS-14: Cria o MovimentacaoForm para registrar entradas/saídas.

--- estoque/forms.py
+++ estoque/forms.py
@@ -1,14 +1,28 @@
 from django import forms
-from .models import Item
+from .models import Item, Movimentacao # Importa Movimentacao
 
 class ItemForm(forms.ModelForm):
     """
-    Formulário para criar e editar um Item, incluindo o Estoque Mínimo (HS-11).
+    Formulário para criar e editar um Item.
     """
     class Meta:
         model = Item
-        # HS-11: Novo campo 'estoque_minimo' incluído
         fields = ['nome', 'descricao', 'quantidade', 'localizacao', 'estoque_minimo'] 
         widgets = {
             'descricao': forms.Textarea(attrs={'rows': 4}),
+        }
+
+# NOVO FORMULÁRIO (HS-14)
+class MovimentacaoForm(forms.ModelForm):
+    """
+    Formulário para registrar entradas e saídas.
+    """
+    class Meta:
+        model = Movimentacao
+        # Excluímos 'item' e 'data_movimentacao' que serão preenchidos pela view
+        fields = ['tipo', 'quantidade_movimentada', 'responsavel', 'observacao']
+        labels = {
+            'tipo': 'Tipo de Operação',
+            'quantidade_movimentada': 'Quantidade',
+            'responsavel': 'Responsável',
         }
