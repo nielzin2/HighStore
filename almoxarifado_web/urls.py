1708ed450a969bc42342a972441573d732bc7abe
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-17 14:11:12 -0300

    Update urls.py
    HS-15: Adiciona as URLs de autenticação padrão do Django no urls.py principal.

--- almoxarifado_web/urls.py
+++ almoxarifado_web/urls.py
@@ -1,13 +1,10 @@
-"""
-Definições de URL para o projeto almoxarifado_web.
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Rota para o painel de administração do Django
     path('admin/', admin.site.urls),
+    path('', include('estoque.urls')),
     
-    # Rota principal: Quando o usuário acessa a raiz, ele é encaminhado para as URLs da app 'estoque'
-    path('', include('estoque.urls')), 
+    # HS-15: Adiciona as URLs de autenticação padrão do Django (/accounts/login, /accounts/logout, etc.)
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
