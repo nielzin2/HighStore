b88cf0478e760e3ee7db320c5c2a286e743edfc6
Daniel Henrique de Souza <143954575+nielzin2@users.noreply.github.com>
2025-11-18 13:51:56 -0300

    Refactor template directory path to use pathlib

--- almoxarifado_web/settings.py
+++ almoxarifado_web/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'almoxarifado_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Diretório global de templates
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
